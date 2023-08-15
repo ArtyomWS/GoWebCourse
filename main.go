d33b1a942a2d1998487b0c8aa66ff14860ea3213
ArtemWS <awsdev.zp@gmail.com>
2023-08-15 16:21:44 +0300

    add: staticHandler

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/ArtyomWS/GoWebCourse/controllers"
 	"github.com/ArtyomWS/GoWebCourse/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -16,16 +17,6 @@ func executeTemplate(w http.ResponseWriter, filepath string) {
 		http.Error(w, "There is an error parsing template", http.StatusInternalServerError)
 	}
 	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,9 +31,25 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(notFoundHandler)
 	http.ListenAndServe(":3000", r)
 }
