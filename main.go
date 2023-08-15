d94241b4fa45e4b9607cafb756617dcf51c0401f
ArtemWS <awsdev.zp@gmail.com>
2023-08-15 12:57:23 +0300

    add: home page template

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content Type", "text/html")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("execute template: %v", err)
+		http.Error(w, "There is an error executing template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content Type", "text/html")
+	fmt.Fprint(w, "Contact")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content Type", "text/html")
+	http.Error(w, "Page Not Found", http.StatusNotFound)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(notFoundHandler)
+	http.ListenAndServe(":3000", r)
 }
